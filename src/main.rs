f960cf5365fbfa22be4834b6438755c5486a9cc2
es183923 <72744903+es183923@users.noreply.github.com>
2022-10-27 19:48:57 -0400

    remove funcs for fetching 1 attribute
    there is literally no performance difference here, and results in a lot of unnecessarily duplicated code

--- src/main.rs
+++ src/main.rs
@@ -28,31 +28,21 @@ enum Commands {
 async fn main() -> Result<(), ()> {
     let cli = Cli::parse();
 
+    let playing = get_media_info().await.unwrap_or_else(|_|
+        MediaInfo {
+            title: "No Music Playing".to_owned(),
+            artist: "No Artist".to_owned(),
+            position: 0_i64.human_duration(),
+        }
+    );
+
     match cli.command {
         Some(case) => match case {
-            Commands::Title => {
-                let playing = get_title().await.unwrap_or_else(|_| "No Song Playing".to_owned());
-                println!("{}", playing)
-            }
-            Commands::Artist => {
-                let playing = get_artist().await.unwrap_or_else(|_| "No Artist".to_owned());
-                println!("{}", playing)
-            }
-            Commands::Position => {
-                let playing = get_position().await.unwrap_or_else(|_| 0.human_duration());
-                println!("{}", playing)
-            }
+            Commands::Title => println!("{}", playing.title),
+            Commands::Artist => println!("{}", playing.artist),
+            Commands::Position => println!("{}", playing.position),
         },
-        None => {
-            let playing = get_media_info().await.unwrap_or_else(|_|
-                MediaInfo {
-                    title: "No Music Playing".to_owned(),
-                    artist: "No Artist".to_owned(),
-                    position: 0_i64.human_duration(),
-                }
-            );
-            println!("{}", playing)
-        }
+        None => println!("{}", playing)
     }
     Ok(())
 }
@@ -65,11 +55,14 @@ async fn get_session() -> Result<GlobalSystemMediaTransportControlsSession, wind
 
 async fn get_media_info() -> Result<MediaInfo, windows::core::Error> {
     let current_session = get_session().await?;
+
     let properties = current_session.TryGetMediaPropertiesAsync()?.await?;
     let title = properties.Title()?;
     let artist = properties.Artist()?;
+
     let timeline = current_session.GetTimelineProperties()?;
     let position = timeline.Position()?;
+
     Ok(MediaInfo {
         title: title.to_string(),
         artist: artist.to_string(),
@@ -77,23 +70,3 @@ async fn get_media_info() -> Result<MediaInfo, windows::core::Error> {
     })
 }
 
-async fn get_artist() -> Result<String, windows::core::Error> {
-    let current_session = get_session().await?;
-    let properties = current_session.TryGetMediaPropertiesAsync()?.await?;
-    let artist = properties.Artist()?;
-    Ok(artist.to_string())
-}
-
-async fn get_position() -> Result<HumanDurationData, windows::core::Error> {
-    let current_session = get_session().await?;
-    let timeline = current_session.GetTimelineProperties()?;
-    let position = timeline.Position()?;
-    Ok((position.Duration / 10_i64.pow(7)).human_duration())
-}
-
-async fn get_title() -> Result<String, windows::core::Error> {
-    let current_session = get_session().await?;
-    let properties = current_session.TryGetMediaPropertiesAsync()?.await?;
-    let title = properties.Title()?;
-    Ok(title.to_string())
-}
